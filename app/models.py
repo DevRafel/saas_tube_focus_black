4f77a9efd5fa58023c17fdd804f24b0ec74fa6fa
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2024-02-22 19:40:50 -0300

    Mudei o nome do SubPasta -> Pasta para id_pasta_pai

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from uuid import uuid1
 
-# Create your models here.
+# Create your models here.  
 
 class Pasta(models.Model):
        id_pasta = models.UUIDField(primary_key=True, default=uuid1, editable=False)
