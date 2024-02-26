be7767260d57f80a153ea4e1ec0d17a30399fb27
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2024-02-26 15:42:11 -0300

    Mudando o uuid1 do id_subpasta para o shortuuid para pegar um id menor para a subpasta

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import shortuuid
 from uuid import uuid1
 
 # Create your models here.  
@@ -9,7 +10,7 @@ class Pasta(models.Model):
        
        
 class SubPasta(models.Model):
-       id_subpasta = models.UUIDField(primary_key = True, default=uuid1, editable=False)
+       id_subpasta = models.CharField(max_length=22, primary_key=True, default=shortuuid.uuid, editable=False)
        nome = models.CharField(max_length=20, default='NEW ARQUIVOS' )
        descricao = models.TextField(default='Description')
        id_pasta_pai = models.ForeignKey(Pasta, on_delete=models.CASCADE, related_name='subpastas')
