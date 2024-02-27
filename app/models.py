04158133f27c9eab3460bc9b250270e486f8640f
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2024-02-27 12:12:30 -0300

    Defilindo arquivos null para o banco de dados BoardApp

--- app/models.py
+++ app/models.py
@@ -15,12 +15,16 @@ class SubPasta(models.Model):
        descricao = models.TextField(default='Description')
        id_pasta_pai = models.ForeignKey(Pasta, on_delete=models.CASCADE, related_name='subpastas')
        
+import shortuuid
+from django.db import models
+
 class BoardApp(models.Model):
-       id_board_app = models.CharField(max_length=22, primary_key=True, default=shortuuid.uuid, editable=False)
-       url_youtube = models.CharField(max_length=100,)
-       anotacoes = models.TextField(default='anoracoes')
-       transcricao = models.TextField(default='transcricao')
-       resumo = models.TextField(default='resumo')
-       ai = models.TextField(default='ai')
-       min_importante = models.CharField(max_length=8)
-       id_subpasta_pai = models.ForeignKey(SubPasta, on_delete=models.CASCADE, related_name='boardapp')    
+    id_board_app = models.CharField(max_length=22, primary_key=True, default=shortuuid.uuid, editable=False)
+    url_youtube = models.CharField(max_length=100, blank=True, null=True)
+    anotacoes = models.TextField(default='anotacoes', blank=True, null=True)
+    transcricao = models.TextField(default='transcricao', blank=True, null=True)
+    resumo = models.TextField(default='resumo', blank=True, null=True)
+    ai = models.TextField(default='ai', blank=True, null=True)
+    min_importante = models.CharField(max_length=8, blank=True, null=True)
+    id_subpasta_pai = models.ForeignKey(SubPasta, on_delete=models.CASCADE, related_name='boardapp', blank=True, null=True)
+  
